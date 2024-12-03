0d8354b50e0fc59cf2824467b2cfec7e2c55df46
fabiele-ghb <fabiele.pine@escola.pr.gov.br>
2024-12-03 19:57:36 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -12,6 +12,11 @@ async function quantidadeUsuarios() {
           type: 'bar'
         }
       ]
+
+    const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
    }
 
   quantidadeUsuarios()
