143b0196e487d88b53d73133d884825a06e28a03
fabiele-ghb <fabiele.pine@escola.pr.gov.br>
2024-12-03 19:27:13 -0300

    fa

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -2,8 +2,16 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
       
-      console.log(dados)
-  }
+    const data = [
+        {
+          x: nomeDasRedes,
+          y: quantidadeUsuarios,
+          type: 'bar'
+        }
+      ]
+   }
 
   quantidadeUsuarios()
